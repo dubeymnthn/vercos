cced22eefe4bb2b786bd68149b18082fa7c52332
manthan <manthandubey5@gmail>
2022-09-02 20:45:35 +0530

    new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   public/index.html 	new file:   src/App.js 	new file:   src/Containers/Renderer/index.js 	new file:   src/button.css 	new file:   src/edges.js 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/nodes.js

---
+++ src/edges.js
@@ -0,0 +1,1 @@
+export default [{ id: "a-b", source: "a", target: "b" }];
---
+++ src/nodes.js
@@ -0,0 +1,26 @@
+export default [
+    {
+      id: "a",
+      type: "input",
+      data: { label: "Node A" },
+      position: { x: 250, y: 25 }
+    },
+    {
+      id: "d",
+      data: { label: "NOde C" },
+      position: { x: 10, y: 50 }
+    },
+  
+    {
+      id: "b",
+      data: { label: "Node B" },
+      position: { x: 100, y: 125 }
+    },
+    {
+      id: "c",
+      type: "output",
+      data: { label: "Node C" },
+      position: { x: 250, y: 250 }
+    }
+  ];
+  
